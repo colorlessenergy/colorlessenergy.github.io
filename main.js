617c8455f253cca80696eae51e8bc1ac028139f0
colorlessenergy <brianmunozdev@gmail.com>
2017-04-14 13:23:23 -0700

    completed nav

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-var slideShowContainer = documen.getElementById("slideshow");
+var navbar = document.getElementById("responsive-navbar");
+var navClick = document.getElementById("nav-click");
+
+navClick.addEventListener("click", function () {
+    navbar.classList.toggle("show");
+});
