903170711c0bdd1725e99cdb43a618622e187162
colorlessenergy <brianmunozdev@gmail.com>
2017-05-29 09:21:16 -0700

    added transition to bg images

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var buttons = document.getElementsByClassName("menu-icon");
 var responsivenav = document.getElementById("responsivenav");
+var bgPics = document.getElementsByClassName('bg');
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
@@ -7,3 +8,12 @@ for (var i = 0; i < buttons.length; i++) {
     responsivenav.classList.toggle("toggle-nav");
   })
 }
+
+document.addEventListener("scroll", function() {
+
+  if (window.pageYOffset > 250) {
+    for (var i = 0; i < bgPics.length; i++) {
+      bgPics[i].style.opacity = "1";
+    }
+  }
+});
