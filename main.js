686ca6272cc2cf2c3ca1d0837abb340e09220a04
colorlessenergy <brianmunozdev@gmail.com>
2022-05-27 09:48:07 -0700

    remove theme change animation

--- main.js
+++ main.js
@@ -13,14 +13,13 @@ if (localStorage.getItem('theme') === 'dark') {
 
 const lightThemeIcon = document.querySelector('#light-theme-icon');
 const darkThemeIcon = document.querySelector('#dark-theme-icon');
-
 const popAudio = new Audio('/pop.mp3');
+
 lightThemeIcon.addEventListener('click', function () {
   popAudio.pause();
   popAudio.currentTime = 0;
   popAudio.play();
   localStorage.setItem('theme', 'light');
-  animateBackgroundColor('#ffffff');
   document.body.classList.remove('dark');
 });
 
@@ -29,7 +28,6 @@ darkThemeIcon.addEventListener('click', function () {
   popAudio.currentTime = 0;
   popAudio.play();
   localStorage.setItem('theme', 'dark');
-  animateBackgroundColor('#333333');
   document.body.classList.add('dark');
 });
 
@@ -59,43 +57,4 @@ const contactMenu = document.querySelector('.contact');
 function contactToView() {
   contactMenu.classList.toggle('contact--back');
   document.body.classList.toggle('disable-scroll');
-}
-
-const squareSize = 50;
-let previousInterval = null;
-function animateBackgroundColor (fillStyle) {
-  if (previousInterval) {
-    clearInterval(previousInterval);
-    previousInterval = null;
-  }
-
-  const canvas = document.querySelector('#animate-background');
-  const context = canvas.getContext('2d');
-  context.fillStyle = fillStyle;
-  let x = 0;
-  let y = 0;
-  let direction = 1;
-
-  const drawBackgroundColorInterval = window.setInterval(() => {
-    context.fillRect(x, y, squareSize, squareSize);
-    y += (squareSize * direction);
-
-    if (y >= canvas.height || (direction === -1 && y < 0)) {
-      direction = direction * -1;
-      if (direction === -1) {
-        y = canvas.height;
-      } else if (direction === 1) {
-        y = 0;
-      }
-
-      x += squareSize;
-
-      if (x >= canvas.width) {
-        document.body.style.backgroundColor = fillStyle;
-        clearInterval(drawBackgroundColorInterval);
-      }
-    }
-  }, 50);
-
-  previousInterval = drawBackgroundColorInterval;
 }
