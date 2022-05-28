0b380379ea9610c42b5dcda14c74c3d7933e3280
colorlessenergy <brianmunozdev@gmail.com>
2022-05-28 07:03:11 -0700

    remove contact me section

--- main.js
+++ main.js
@@ -37,23 +37,3 @@ menuHamburger.addEventListener('click', function () {
   responsiveNav.classList.toggle('mobile-nav--active');
   document.body.classList.toggle('disable-scroll');
 });
-
-const contactNav = document.querySelector('.contact__nav');
-if (contactNav) {
-  contactNav.addEventListener('click', function (event) {
-    if (event.target.className.includes('contact__back')) {
-      contactToView();
-    }
-  });
-}
-
-const svgContactButton = document.querySelector('.mail-icon');
-if (svgContactButton) {
-  svgContactButton.addEventListener('click', contactToView);
-}
-
-const contactMenu = document.querySelector('.contact');
-function contactToView() {
-  contactMenu.classList.toggle('contact--back');
-  document.body.classList.toggle('disable-scroll');
-}
