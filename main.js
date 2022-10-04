2bbc882057fc93ea682aef794d19f87e8cf948d9
colorlessenergy <brianmunozdev@gmail.com>
2022-10-03 20:13:34 -0700

    ran prettier

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 if (!localStorage.getItem('theme')) {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
+    if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 }
 
 if (localStorage.getItem('theme') === 'dark') {
-  document.body.classList.add('dark');
+    document.body.classList.add('dark');
 }
 
 const lightThemeIcon = document.querySelector('#light-theme-icon');
@@ -15,25 +18,25 @@ const darkThemeIcon = document.querySelector('#dark-theme-icon');
 const popAudio = new Audio('/pop.mp3');
 
 lightThemeIcon.addEventListener('click', function () {
-  popAudio.pause();
-  popAudio.currentTime = 0;
-  popAudio.play();
-  localStorage.setItem('theme', 'light');
-  document.body.classList.remove('dark');
+    popAudio.pause();
+    popAudio.currentTime = 0;
+    popAudio.play();
+    localStorage.setItem('theme', 'light');
+    document.body.classList.remove('dark');
 });
 
 darkThemeIcon.addEventListener('click', function () {
-  popAudio.pause();
-  popAudio.currentTime = 0;
-  popAudio.play();
-  localStorage.setItem('theme', 'dark');
-  document.body.classList.add('dark');
+    popAudio.pause();
+    popAudio.currentTime = 0;
+    popAudio.play();
+    localStorage.setItem('theme', 'dark');
+    document.body.classList.add('dark');
 });
 
 const menuHamburger = document.querySelector('#menu-hamburger');
 const responsiveNav = document.querySelector('.mobile-nav');
 menuHamburger.addEventListener('click', function () {
-  menuHamburger.classList.toggle('animate');
-  responsiveNav.classList.toggle('mobile-nav--active');
-  document.body.classList.toggle('disable-scroll');
+    menuHamburger.classList.toggle('animate');
+    responsiveNav.classList.toggle('mobile-nav--active');
+    document.body.classList.toggle('disable-scroll');
 });
