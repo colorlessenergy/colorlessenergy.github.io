152fe36ee64301f873b2749e7b9a2a444e617566
colorlessenergy <brianmunozdev@gmail.com>
2022-10-03 20:37:09 -0700

    add animation to project image when hovering project

--- main.js
+++ main.js
@@ -40,3 +40,46 @@ menuHamburger.addEventListener('click', function () {
     responsiveNav.classList.toggle('mobile-nav--active');
     document.body.classList.toggle('disable-scroll');
 });
+
+const playImages = (image, container, amountOfImages) => {
+    const imageElement = document.querySelector(`.${image}`);
+    if (!imageElement) {
+        return;
+    }
+
+    const originalImagSrc = imageElement.src;
+    const containerElement = document.querySelector(container);
+    let timeouts = [];
+    const animate = () => {
+        for (let i = 1; i <= amountOfImages; i++) {
+            let timeoutID = setTimeout(() => {
+                if (i === amountOfImages) {
+                    animate();
+                }
+
+                imageElement.src = `/images/${image}/${image}-${i}.png`;
+
+                timeouts.shift();
+            }, 800 * i);
+
+            timeouts.push(timeoutID);
+        }
+    };
+
+    containerElement.addEventListener('mouseenter', () => {
+        animate();
+    });
+
+    containerElement.addEventListener('mouseleave', () => {
+        imageElement.src = originalImagSrc;
+
+        timeouts.forEach(timeoutID => {
+            clearTimeout(timeoutID);
+        });
+        timeouts = [];
+    });
+};
+
+playImages('productivity', '.productivity-container', 6);
+playImages('place', '.place-container', 2);
+playImages('tracker', '.tracker-container', 3);
